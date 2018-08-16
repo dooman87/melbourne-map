ed4e62ff5f521cb40955f5717f40e095b9d3d2dc
zstadler <zeev.stadler@gmail.com>
2018-08-16 19:55:25 +0300

    Use https where appropriate

--- download.sh
+++ download.sh
@@ -14,7 +14,7 @@ mkdir -p ${SHAPES_DIR}/water-polygons-split-3857
 
 # world_boundaries
 echo "downloading world_boundaries..."
-curl -z "${SHAPES_DIR}/world_boundaries-spherical.tgz" -L -o "${SHAPES_DIR}/world_boundaries-spherical.tgz" "http://planet.openstreetmap.org/historical-shapefiles/world_boundaries-spherical.tgz"
+curl -z "${SHAPES_DIR}/world_boundaries-spherical.tgz" -L -o "${SHAPES_DIR}/world_boundaries-spherical.tgz" "https://planet.openstreetmap.org/historical-shapefiles/world_boundaries-spherical.tgz"
 echo "expanding world_boundaries..."
 tar -xzf ${SHAPES_DIR}/world_boundaries-spherical.tgz -C ${SHAPES_DIR}/
 
@@ -32,7 +32,7 @@ unzip $UNZIP_OPTS ${SHAPES_DIR}/simplified-water-polygons-complete-3857.zip \
 
 # ne_110m_admin_0_boundary_lines_land
 echo "downloading ne_110m_admin_0_boundary_lines_land..."
-curl -z ${SHAPES_DIR}/ne_110m_admin_0_boundary_lines_land.zip -L -o ${SHAPES_DIR}/ne_110m_admin_0_boundary_lines_land.zip http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_boundary_lines_land.zip
+curl -z ${SHAPES_DIR}/ne_110m_admin_0_boundary_lines_land.zip -L -o ${SHAPES_DIR}/ne_110m_admin_0_boundary_lines_land.zip https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_boundary_lines_land.zip
 echo "expanding ne_110m_admin_0_boundary_lines_land..."
 unzip $UNZIP_OPTS ${SHAPES_DIR}/ne_110m_admin_0_boundary_lines_land.zip \
   ne_110m_admin_0_boundary_lines_land.shp \
